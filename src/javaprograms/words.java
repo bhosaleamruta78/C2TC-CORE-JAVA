74a13c63489e6cf67a4b1151c747a440805d7407
Admin <Admin@DESKTOP-GVOLAUV>
2023-12-22 02:17:00 +0530

    number of words in string

---
+++ src/javaprograms/words.java
@@ -0,0 +1,18 @@
+package javaprograms;
+
+public class words {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name="JavaPrograms";
+		
+		char[] tempArray=name.toCharArray();
+		
+		int n=tempArray.length;
+		
+		System.out.println("Number of words in string "+name+" are: "+n);
+
+	}
+
+}
